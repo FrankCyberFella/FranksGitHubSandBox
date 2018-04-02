2fb550e9a8f02af48c477a3dd9969840d0b331c7
Frank@techelevator.com <frank@cyberacademy.com>
2018-04-02 19:43:59 +0000

    SamplPgm.cbl edited online with Bitbucket

--- Code/COBOL-Stuff/SamplPgm.cbl
+++ Code/COBOL-Stuff/SamplPgm.cbl
@@ -3,7 +3,7 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-         01 NUMBER-1      PIC 99.
+         01 NUMBER-1               PIC 999.
          
         PROCEDURE DIVISION.
             DISPLAY 'GOOD MORNING FRANK!'.
